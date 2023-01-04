78e919c04ab0e65380fb416afd6180079dd9721f
zaiic <zaiic@qq.com>
2023-01-04 22:40:31 +0800

    fix: fixed the 'on_attach' configuration
    When programming in Rust, the 'on_attach' configed by 'lsp server' could not work.
    Because the plugin 'rust-tools.nvim' will overwirte the 'on_attach' option,
    we have to re-write the same code in the 'conf/rust-tools-conf.lua'.

--- lua/conf/rust-tools-conf.lua
+++ lua/conf/rust-tools-conf.lua
@@ -1,6 +1,28 @@
 local function on_attach(client, buffer)
   -- This callback is called when the LSP is atttached/enabled for this buffer
   -- we could set keymaps related to LSP, etc here.
+    local bufopts = { noremap = true, silent = true, buffer = buffer }
+
+   -- 跳转到定义（代替内置 LSP 的窗口，telescope 插件让跳转定义更方便）
+    vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions theme=dropdown<CR>", bufopts)
+    -- 列出光标下所有引用（代替内置 LSP 的窗口，telescope 插件让查看引用更方便）
+    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references theme=dropdown<CR>", bufopts)
+    -- 工作区诊断（代替内置 LSP 的窗口，telescope 插件让工作区诊断更方便）
+    vim.keymap.set("n", "go", "<cmd>Telescope diagnostics theme=dropdown<CR>", bufopts)
+    -- 显示代码可用操作（代替内置 LSP 的窗口，telescope 插件让代码行为更方便）
+    vim.keymap.set("n", "<leader>ca", "<cmd>Telescope lsp_code_actions theme=dropdown<CR>", bufopts)
+    -- 变量重命名（代替内置 LSP 的窗口，Lspsaga 让变量重命名更美观）
+    vim.keymap.set("n", "<leader>cn", "<cmd>Lspsaga rename<CR>", bufopts)
+    -- 查看帮助信息（代替内置 LSP 的窗口，Lspsaga 让查看帮助信息更美观）
+    vim.keymap.set("n", "gh", "<cmd>Lspsaga hover_doc<CR>", bufopts)
+    -- 跳转到上一个问题（代替内置 LSP 的窗口，Lspsaga 让跳转问题更美观）
+    vim.keymap.set("n", "g[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", bufopts)
+    -- 跳转到下一个问题（代替内置 LSP 的窗口，Lspsaga 让跳转问题更美观）
+    vim.keymap.set("n", "g]", "<cmd>Lspsaga diagnostic_jump_next<CR>", bufopts) 
+    -- 悬浮窗口上翻页，由 Lspsaga 提供
+    vim.keymap.set("n", "<C-p>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", bufopts)
+    -- 悬浮窗口下翻页，由 Lspsaga 提供
+    vim.keymap.set("n", "<C-n>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", bufopts)
 end
 
 -- Configure LSP through rust-tools.nvim plugin.
