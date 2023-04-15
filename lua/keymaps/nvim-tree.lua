66fdaed483f1cc31ae79761457b3a8affca85d85
zaiic <zaiic@outlook.com>
2023-04-15 22:37:04 +0800

    feat: disable 'e', 'f' and 'F' keymaps in Normal Mode in nvim-tree

---
+++ lua/keymaps/nvim-tree.lua
@@ -0,0 +1,17 @@
+local function attach(bufnr)
+  local api = require('nvim-tree.api')
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', 'e', '', { buffer = bufnr })
+  vim.keymap.del('n', 'e', { buffer = bufnr })
+  vim.keymap.set('n', 'F', '', { buffer = bufnr })
+  vim.keymap.del('n', 'F', { buffer = bufnr })
+  vim.keymap.set('n', 'f', '', { buffer = bufnr })
+  vim.keymap.del('n', 'f', { buffer = bufnr })
+end
+
+return attach
--- lua/lazy/conf/mod.lua
+++ lua/lazy/conf/mod.lua
@@ -1,4 +1,8 @@
 require('lualine').setup()
 require('Comment').setup()
 require('fidget').setup()
-require('nvim-tree').setup()
+
+local nvim_tree_attach = require('keymaps.nvim-tree')
+require('nvim-tree').setup({
+  on_attach = nvim_tree_attach
+})
