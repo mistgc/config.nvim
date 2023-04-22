c655020888a7f5c8738f125e5dec376455a8bd4d
zaiic <zaiic@outlook.com>
2023-04-22 09:15:53 +0800

    fix: add missing configuration file for 'nvim-treesitter'

--- lua/lazy/conf/mod.lua
+++ lua/lazy/conf/mod.lua
@@ -1,6 +1,7 @@
 require('lualine').setup()
 require('Comment').setup()
 require('fidget').setup()
+require('lazy.conf.treesitter')
 
 local nvim_tree_attach = require('keymaps.nvim-tree')
 require('nvim-tree').setup({
---
+++ lua/lazy/conf/treesitter.lua
@@ -0,0 +1,17 @@
+require'nvim-treesitter.configs'.setup {
+  -- ensure_installed = { "c", "lua", "rust", "python", "typescript", "cpp", "javascript" },
+  sync_install = false,
+  auto_install = false,
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
