c974dde1854e26c05c12d1cfd873532308e738fc
zaiic <zaiic@outlook.com>
2023-07-12 18:51:38 +0800

    chore: update plugins and add config for 'nvim-tree'

--- lua/lazy/conf/mod.lua
+++ lua/lazy/conf/mod.lua
@@ -4,8 +4,4 @@ require("fidget").setup()
 require("lazy.conf.treesitter")
 require("lazy.conf.mason-null-ls")
 require("lazy.conf.gitsigns")
-
-local nvim_tree_attach = require("keymaps.nvim-tree")
-require("nvim-tree").setup({
-  on_attach = nvim_tree_attach,
-})
+require("lazy.conf.nvim-tree")
---
+++ lua/lazy/conf/nvim-tree.lua
@@ -0,0 +1,73 @@
+local nvim_tree_attach = require("keymaps.nvim-tree")
+require("nvim-tree").setup({
+  on_attach = nvim_tree_attach,
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_tab = false,
+  hijack_cursor = true,
+  hijack_unnamed_buffer_when_opening = false,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+  },
+  view = {
+    adaptive_size = false,
+    side = "left",
+    width = 25,
+  },
+  git = {
+    enable = false,
+    ignore = true,
+  },
+  filesystem_watchers = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
+    },
+  },
+  renderer = {
+    root_folder_label = false,
+    highlight_git = false,
+    highlight_opened_files = "none",
+
+    indent_markers = {
+      enable = false,
+    },
+
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = false,
+      },
+
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+})
