db376a4e15b420605f9706b5370c74018bcd0d49
zaiic <zaiic@outlook.com>
2023-08-21 01:09:35 +0800

    feat: add 'tree-sitter-just' for highlighting for 'justfile's.

--- lua/lazy/conf/treesitter.lua
+++ lua/lazy/conf/treesitter.lua
@@ -15,3 +15,13 @@ require("nvim-treesitter.configs").setup({
     additional_vim_regex_highlighting = false,
   },
 })
+
+require("nvim-treesitter.parsers").get_parser_configs().just = {
+  install_info = {
+    url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main",
+    -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+  },
+  maintainers = { "@IndianBoy42" },
+}
