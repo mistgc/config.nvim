a0512c03160c87c61db6e050c1339dab275d96cc
zaiic <zaiic@outlook.com>
2024-01-19 23:37:59 +0800

    feat: add the plugin 'gx.nvim'

--- lua/lazy/plugins.lua
+++ lua/lazy/plugins.lua
@@ -95,4 +95,30 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = true,
   },
+  {
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+      vim.g.netrw_nogx = 1
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+
+    config = function()
+      require("gx").setup({
+        open_browser_app = "xdg-open",
+        handlers = {
+          plugin = true,
+          github = true,
+          brewfile = true,
+          package_json = true,
+          search = true,
+        },
+        handler_options = {
+          search_engine = "bing",
+        },
+      })
+    end,
+  },
 }
