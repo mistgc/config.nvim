86a080d4367cb21c08c8bdee3f484abc014ae171
mistgc <georgecai0908@outlook.com>
2024-02-27 12:27:54 +0800

    !feat(keymap): add support for Qwerty Layout

--- lua/keymaps/local.lua
+++ lua/keymaps/local.lua
@@ -6,20 +6,11 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Vim for Colemak
-map("", "n", "j", {})
-map("", "e", "k", {})
-map("", "i", "l", {})
-map("", "j", "e", {})
-map("", "k", "n", {})
-map("", "l", "i", {})
-map("", "K", "N", {})
-map("", "N", "5j", {})
-map("", "E", "5k", {})
-map("", "L", "I", {})
-
+-- Vim for Qwerty
 map("", "H", "0", {})
-map("", "I", "$", {})
+map("", "L", "$", {})
+map("", "J", "5j", {})
+map("", "K", "5k", {})
 
 -- Shortcut for quiting and saving
 map("", "Q", ":q<cr>", {})
