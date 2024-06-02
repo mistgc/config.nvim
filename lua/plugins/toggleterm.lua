4e50913886c00be847b4394d4035062aa9506db1
mistgc <georgecai0908@outlook.com>
2024-06-02 18:11:33 +0800

    fix: modify options of "toggleterm.nvim" to support Windows OS

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,19 +1,28 @@
-local sys_name = vim.loop.os_uname().sysname
-local sh_exec = os.getenv("SHELL")
+local function config()
+  local toggleterm = require("toggleterm")
+  local sys_name = tostring(vim.loop.os_uname().sysname)
+  local sh_exec = nil
 
-if sh_exec == nil then
-  local log_warn = require("utils").log_warn
-  log_warn('Because the SHELL is nil, "toggleterm.nvim" use default shell.', "ToggleTerm: ")
+  if sys_name:lower():find("windows", 1) > 0 then
+    sh_exec = "pwsh"
+  else
+    sh_exec = os.getenv("SHELL")
+    if sh_exec == nil then
+      local log_warn = require("utils").log_warn
+      log_warn('Because the SHELL is nil, the "toggleterm.nvim" will use the defualt shell.', "ToggleTerm: ")
+    end
+  end
+
+  toggleterm.setup({
+    shell = sh_exec,
+    shade_terminals = false,
+  })
 end
 
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
-  opts = {
-    shade_terminals = false,
-    shell = sh_exec,
-  },
+  config = config,
   keys = {
     { "<C-t>", "<cmd>ToggleTerm<cr>", mode = { "n" }, desc = "Toggle terminal." },
     { "<ESC>", "<C-\\><C-n>", mode = { "t" }, desc = "Escape the terminal mode." },
