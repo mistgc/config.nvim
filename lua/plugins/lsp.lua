1482388c60cd2429c5b41b5562cdedf80b9aee04
mistgc <georgecai0908@outlook.com>
2024-06-04 13:14:21 +0800

    feat: disable the "lightbulb"

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,9 +99,11 @@ return {
       {
         "nvimdev/lspsaga.nvim",
         event = "LspAttach",
-        config = function()
-          require("lspsaga").setup()
-        end,
+        opts = {
+          lightbulb = {
+            enable = false,
+          },
+        },
         dependencies = {
           "nvim-treesitter/nvim-treesitter",
           "nvim-tree/nvim-web-devicons",
