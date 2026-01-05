939de04f3fe38681d9b2dbc5bec5d3dc6245f84b
mistgc <georgecai0908@outlook.com>
2026-01-05 13:48:05 +0800

    feat: add remark_ls LSP and grp plugin
    - Enable remark_ls LSP server for markdown support
    - Add grp.lua plugin in development mode from local path
    - Update .gitignore to exclude dev.lua plugin file

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,5 @@
 vim.lsp.enable("lua_ls")
+vim.lsp.enable("remark_ls")
 vim.lsp.enable("clangd")
 vim.lsp.enable("pyright")
 vim.lsp.enable("rust_analyzer")
---
+++ lua/plugins/grp.lua
@@ -0,0 +1,5 @@
+return {
+  -- 'mistgc/grp.nvim',
+  dir = "/home/gc/workspace/proj/grp.nvim/",
+  opts = {},
+}
