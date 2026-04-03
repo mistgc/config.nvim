e0d9454a09c45b1a2ff1526a36c761d6efe21468
mistgc <georgecai0908@outlook.com>
2026-04-03 23:56:24 +0800

    refactor: consolidate LSP configurations and add ts_ls support

--- lsp/cmake.lua
+++
@@ -1,8 +0,0 @@
-return {
-  cmd = { "cmake-language-server" },
-  filetypes = { "cmake" },
-  root_markers = { "CMakePresets.json", "CTestConfig.cmake", ".git", "build", "cmake" },
-  init_options = {
-    buildDirectory = "build",
-  },
-}
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,4 @@
+return {
+  "neovim/nvim-lspconfig",
+  -- opts = {},
+}
